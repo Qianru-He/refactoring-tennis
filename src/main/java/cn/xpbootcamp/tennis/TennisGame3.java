305e79866e7250fe1b1e4645cba98b0cdbd416cd
Qianru-He <2583543607@qq.com>
2020-03-03 15:23:02 +0800

    extract method for game3

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -1,37 +1,44 @@
 package cn.xpbootcamp.tennis;
 
+
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+	private int p2Score;
+	private int p1Score;
+	private String p1Name;
+	private String p2Name;
+	private String[] possibleScores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
-    }
+	public TennisGame3(String p1N, String p2N) {
+		this.p1Name = p1N;
+		this.p2Name = p2N;
+	}
 
-    public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
-        }
-    }
+	public String getScore() {
+		if (p1Score < 4 && p2Score < 4 && !(p1Score + p2Score == 6)) {
+			return (p1Score == p2Score) ? translateScore(p1Score) + "-All" : translateScore(p1Score) + "-" + translateScore(p2Score);
+		} else if (p1Score == p2Score) {
+			return "Deuce";
+		} else {
+			String winer = p1Score > p2Score ? p1Name : p2Name;
+			return isAdvantage() ? "Advantage " + winer : "Win for " + winer;
+		}
+	}
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
-        else
-            this.p2 += 1;
+	private boolean isAdvantage() {
+		return (p1Score - p2Score) * (p1Score - p2Score) == 1;
+	}
 
-    }
+	private String translateScore(int score) {
+		return possibleScores[score];
+	}
+
+	public void wonPoint(String playerName) {
+		if (playerName.equals("player1"))
+			this.p1Score += 1;
+		else
+			this.p2Score += 1;
+
+	}
 
 }
